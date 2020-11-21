74e569a582a8003f255fdd530d0b179f210d00aa
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-11-21 20:41:29 +0100

    Getting user input

--- src/App.java
+++ src/App.java
@@ -1,18 +1,30 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String baseUrl = "https://www.nba.com/stats/";
-		// WebClient client = new WebClient();
+		String baseUrl = "https://www.basketball-reference.com/leagues/NBA_2020_per_game.html";
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter player name: ");
+		
+		String playerName = userInput.nextLine();
+		// System.out.println(playerName);
 		try {
 			Document doc = Jsoup.connect(baseUrl).get();
 			
-			System.out.printf("Title: %s\n", doc.title());
+			Elements playerPage = doc.getElementsByAttribute("data-append-csv");
+			
+			// System.out.printf("Title: %s\n", doc.title());
+			// System.out.println(doc.outerHtml());
+			// System.out.println(playerPage);
 		} catch (IOException event) {
 			event.printStackTrace();
 		}
