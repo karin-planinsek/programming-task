bc62b8d47920c2e8e8127ff5d4f85e83bd9805f9
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-11-21 17:41:17 +0100

    Importing jsoup library and outputing title of the website

--- src/App.java
+++ src/App.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String baseUrl = "https://www.nba.com/stats/";
 		// WebClient client = new WebClient();
+		try {
+			Document doc = Jsoup.connect(baseUrl).get();
+			
+			System.out.printf("Title: %s\n", doc.title());
+		} catch (IOException event) {
+			event.printStackTrace();
+		}
+		
 		
 	}
 
