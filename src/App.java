c8c7653e571a34b8fc6d22adbf90103f49408290
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-11-22 21:44:13 +0100

    Code cleanup

--- src/App.java
+++ src/App.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class App {
 
@@ -20,20 +18,14 @@ public class App {
 		
 		System.out.println("Getting three point average for every season for player: " + wantedPlayer);
 		
-
 		try {
 			Document doc = Jsoup.connect(baseUrl).get();
 			
 			Boolean playerFound = false;
-			// Elements playerPage = doc.getElementsByAttribute("data-append-csv");
 			
 			for (Element player: doc.getElementsByAttribute("data-append-csv")) {
-				// String playerName = player.text();
-				// System.out.println(playerName);
 				
 				String playerName = player.text();
-				// System.out.println(playerName);
-				
 				
 				if (wantedPlayer.equals(playerName)) {
 					String url = player.select("a").attr("href");
@@ -43,7 +35,6 @@ public class App {
 					playerFound = true;
 					
 					Document playerPage = Jsoup.connect(playerUrl).get();
-					// System.out.println(playerPage.outerHtml());
 					
 					for (Element page: playerPage.select("table#per_game tr")) {
 						String season = page.select("th.left").text();
@@ -61,16 +52,6 @@ public class App {
 			if (playerFound.equals(false)) {
 				System.out.println("No player with name " + wantedPlayer + " found");
 			}
-			
-			
-			
-			
-			 //System.out.printf("Title: %s\n", doc.title());
-			// System.out.println(doc.outerHtml());
-			
-			
-			
-			
 			
 		} catch (IOException event) {
 			event.printStackTrace();
